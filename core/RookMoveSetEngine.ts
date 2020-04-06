cf9a264bea05b31d10172fc267b8ea8af6e22bae
Joe <6955350+joebutler2@users.noreply.github.com>
2020-04-06 13:11:34 -0400

    Refactor the MoveSetEngine.

--- core/RookMoveSetEngine.ts
+++ core/RookMoveSetEngine.ts
@@ -18,80 +18,133 @@ export default class RookMoveSetEngine {
   // player is moving.
   public canMoveTo(row: number, column: number, destRow: number, destColumn: number): boolean {
     const team = this.pieces[row][column].team;
+    let iterable;
+    let destination;
     if (column === destColumn) {
-      const iterable = row > destRow
-        ? new MoveUpIterable(row, destRow)
-        : new MoveDownIterable(row, destRow);
-      for (const i of iterable) {
-        if (i === destRow && team !== this.pieces[i][column].team) {
-          return true;
-        }
-        if (!(this.pieces[i][column] instanceof NullPiece)) {
-          return false;
-        }
-      }
+      destination = destRow;
+      iterable = row > destRow
+        ? new MoveUpIterable(row, column, destRow)
+        : new MoveDownIterable(row, column, destRow);
     } else if (row === destRow) {
-      if (column > destColumn) { // Moving Left.
-        for (let i = column - 1; i >= destColumn; i--) {
-          if (i === destColumn && team !== this.pieces[row][i].team) {
-            return true;
-          }
-          if (!(this.pieces[row][i] instanceof NullPiece)) {
-            return false;
-          }
-       }
-      } else { // Moving Right.
-        for (let i = column + 1; i <= destColumn; i++) {
-          if (i === destColumn && team !== this.pieces[row][i].team) {
-            return true;
-          }
-          if (!(this.pieces[row][i] instanceof NullPiece)) {
-            return false;
-          }
-        }
+      destination = destColumn;
+      iterable = column > destColumn
+        ? new MoveLeftIterable(row, column, destColumn)
+        : new MoveRightIterable(row, column, destColumn);
+    } else {
+      return false;
+    }
+
+    for (const {index, row: curRow, column: curColumn} of iterable) {
+      if (index === destination && team !== this.pieces[curRow][curColumn].team) {
+        return true;
+      }
+      if (!(this.pieces[curRow][curColumn] instanceof NullPiece)) {
+        return false;
       }
     }
+
     return false;
   }
 }
 
-class MoveUpIterable implements Iterable<number> {
-  constructor(private index: number, private destRow: number) {}
+class MoveLeftIterable implements Iterable<MovementIteratorResult> {
+  constructor(private row: number,
+              private index: number,
+              private destColumn: number) {}
 
-  public [Symbol.iterator](): MoveUpIterator {
-    return new MoveUpIterator(this.index, this.destRow);
+  public [Symbol.iterator](): MoveLeftIterator {
+    return new MoveLeftIterator(this.row, this.index, this.destColumn);
   }
 }
 
-class MoveUpIterator implements Iterator<number> {
-  constructor(private index: number, private destRow: number) {}
+interface MovementIteratorResult {
+  index: number;
+  row: number;
+  column: number;
+}
 
-  public next(): IteratorResult<number> {
+class MoveLeftIterator implements Iterator<MovementIteratorResult> {
+  constructor(private row: number,
+              private index: number,
+              private destColumn: number) {}
+
+  public next(): IteratorResult<MovementIteratorResult> {
+    this.index--;
+    return {
+      done: this.index < this.destColumn,
+      value: {index: this.index, row: this.row, column: this.index},
+    };
+  }
+}
+
+class MoveRightIterable implements Iterable<MovementIteratorResult> {
+  constructor(private row: number,
+              private index: number,
+              private destColumn: number) {}
+
+  public [Symbol.iterator](): MoveRightIterator {
+    return new MoveRightIterator(this.row, this.index, this.destColumn);
+  }
+}
+
+class MoveRightIterator implements Iterator<MovementIteratorResult> {
+  constructor(private row: number,
+              private index: number,
+              private destColumn: number) {}
+
+  public next(): IteratorResult<MovementIteratorResult> {
+    this.index++;
+    return {
+      done: this.index > this.destColumn,
+      value: {index: this.index, row: this.row, column: this.index},
+    };
+  }
+}
+
+class MoveUpIterable implements Iterable<MovementIteratorResult> {
+  constructor(private index: number,
+              private column: number,
+              private destRow: number) {}
+
+  public [Symbol.iterator](): MoveUpIterator {
+    return new MoveUpIterator(this.index, this.column, this.destRow);
+  }
+}
+
+class MoveUpIterator implements Iterator<MovementIteratorResult> {
+  constructor(private index: number,
+              private column: number,
+              private destRow: number) {}
+
+  public next(): IteratorResult<MovementIteratorResult> {
     this.index--;
     return {
       done: this.index < this.destRow,
-      value: this.index,
+      value: {index: this.index, row: this.index, column: this.column},
     };
   }
 }
 
-class MoveDownIterable implements Iterable<number> {
-  constructor(private index: number, private destRow: number) {}
+class MoveDownIterable implements Iterable<MovementIteratorResult> {
+  constructor(private index: number,
+              private column: number,
+              private destRow: number) {}
 
   public [Symbol.iterator](): MoveDownIterator {
-    return new MoveDownIterator(this.index, this.destRow);
+    return new MoveDownIterator(this.index, this.column, this.destRow);
   }
 }
 
-class MoveDownIterator implements Iterator<number> {
-  constructor(private index: number, private destRow: number) {}
+class MoveDownIterator implements Iterator<MovementIteratorResult> {
+  constructor(private index: number,
+              private column: number,
+              private destRow: number) {}
 
-  public next(): IteratorResult<number> {
+  public next(): IteratorResult<MovementIteratorResult> {
     this.index++;
     return {
       done: this.index > this.destRow,
-      value: this.index,
+      value: {index: this.index, row: this.index, column: this.column},
     };
   }
 }
-
