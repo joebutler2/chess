0512b6771758bb47e8c736a40812624dc2656c9f
Joe <6955350+joebutler2@users.noreply.github.com>
2021-03-09 15:34:39 -0500

    Use === instead of ==.

--- core/pieces/Queen.ts
+++ core/pieces/Queen.ts
@@ -3,7 +3,7 @@ import Team from "../Team";
 
 export default class Queen extends Piece {
   get icon(): string {
-    return this.team == Team.Black ? "♛" : "♕";
+    return this.team === Team.Black ? "♛" : "♕";
   }
 
   get moveSet(): string {
--- core/pieces/Rook.ts
+++ core/pieces/Rook.ts
@@ -3,7 +3,7 @@ import Team from "../Team";
 
 export default class Rook extends Piece {
   get icon(): string {
-    return this.team == Team.Black ? "♜" : "♖";
+    return this.team === Team.Black ? "♜" : "♖";
   }
 
   get moveSet(): string {
